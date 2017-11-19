5de157c9e52d36bfc1f8ff35b88e1d34ebbd64ad
João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2017-11-19 11:42:39 -0200

    Configure postgre in docker

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,6 @@ RUN mkdir /code
 WORKDIR /code
 ADD requirements.txt /code/
 RUN pip install -r requirements.txt && apt-get update -y
+RUN pip install ansible
+RUN DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends apt-utils
 ADD . /code/
--- install_apt.sh
+++ install_apt.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+apt-get update
+apt-get install -y python3-pip
+# apt-get install -y vim
+# apt-get install -y python-virtualenv
+# apt-get install -y virtualenvwrapper
 # apt-get install -y libssl-dev openssl
+# wget https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz
+# tar -xvzf Python-3.5.2.tgz
+# cd Python-3.5.2/
+# ./configure --with-ensurepip=install
+# make
+# sudo make install
 
 # wait for Postgres to start
 postgres_ready(){
@@ -18,12 +29,12 @@ until postgres_ready; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
-
+#
 # echo "Delete migrations"
 # find . -path "*/migrations/*.pyc"  -delete
 # find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-# echo "Create makemigrations and migrate"
-# python3 sas/manage.py makemigrations
-# python3 sas/manage.py migrate
-# echo "Run server"
-# python3 sas/manage.py runserver 0.0.0.0:8000
+echo "Create makemigrations and migrate"
+python3 sas/manage.py makemigrations
+python3 sas/manage.py migrate
+echo "Run server"
+python3 sas/manage.py runserver 0.0.0.0:8000
