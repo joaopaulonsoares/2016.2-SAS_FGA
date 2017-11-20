381d885d055b7a6f0151d5685e411ce44f1d86d7
João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2017-11-20 16:27:50 -0200

    Updating template root

--- sas/sas/settings.py
+++ sas/sas/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'sas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['sas/sas/templates/'],
+        'DIRS': ['sas/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
