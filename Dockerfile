7ea75bedc8cff7faa149b9b33c0c71adf30f0bd5
João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2017-11-20 16:55:33 -0200

    Changing gitignore and adding gulp initial files

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,5 @@ WORKDIR /code
 ADD requirements.txt /code/
 ADD requirements-dev.txt /code/
 RUN pip install -r requirements.txt && apt-get update -y
-RUN pip install ansible
 RUN DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends apt-utils
 ADD . /code/
---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var runsequence = require('run-sequence');
+var reload = browserSync.reload;
+
+var directoryjs = './sas/**/*.js'
+var directorycss = './sas/**/*.css'
+var directoryhtml = './sas/**/*.html'
+
+var dist = "./dist"
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    open: false,
+    notify: false,
+    proxy: 'localhost:8000'
+  })
+});
+
+gulp.task('watch', function() {
+  gulp.watch(directoryjs,['files']);
+  gulp.watch(directoryhtml,['files']);
+  gulp.watch(directorycss,['files']);
+});
+
+gulp.task('reload', function() {
+  gulp.watch(directoryjs).on('change', browserSync.reload);
+  gulp.watch(directoryhtml).on('change', browserSync.reload);
+  gulp.watch(directorycss).on('change', browserSync.reload);
+});
+
+gulp.task('files', function() {
+  return gulp.src(directory)
+    .pipe(gulp.dest(dist));
+});
+
+gulp.task('build', function(cb) {
+  runsequence('files', cb);
+});
+
+gulp.task('default',['browserSync', 'watch','reload']);
